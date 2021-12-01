763185b7a5e8ac87d96c5031aa332a5b9745378a
Mr21percent <54969887+Mr21percent@users.noreply.github.com>
2021-12-01 21:30:23 +0900

    Add files via upload

---
+++ workingfile_without_freezing/final2.py
@@ -0,0 +1,136 @@
+import time
+import Adafruit_DHT as dht
+import RPi.GPIO as GPIO
+
+def checkCurrentHumAndTemp(gpioNum=26):
+    '''
+    센서로부터 정보를 가져오는 함수
+    '''
+    f = open("temphum.txt", 'r')
+    lines = f.readlines()
+    humidity,temp = dht.read_retry(dht.DHT22, gpioNum)
+    #humidity = 10
+    #temp = 25.8
+    if humidity != None:
+        humidity=round(humidity, 1)
+    else:
+        humidity = float(lines[1])
+    if temp != None:
+        temp=round(temp, 1)
+    else:
+        temp = float(lines[0].strip())
+    
+    return humidity, temp
+
+def connectRelay(gpioNum, act=True):
+    '''
+    #every connection of relay is based on NO and COM
+    #if act==True it works : connect NO and COM
+    #if act==False it doesn't works : connect NC and Com
+    '''
+    GPIO.setup(gpioNum, GPIO.OUT)
+    if act==True:
+        GPIO.output(gpioNum, True)
+    if act==False:
+        GPIO.output(gpioNum, False)
+
+
+
+#set gpioNum with BCM -1 = non set
+airPump_relay_gpioNum = 27
+led_relay_gpioNum = 24
+cooler_relay_gpioNum = 17
+led_r_relay_gpioNum = 23
+
+#GPIO setting
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(airPump_relay_gpioNum, GPIO.OUT)
+GPIO.setup(led_relay_gpioNum, GPIO.OUT)
+GPIO.setup(cooler_relay_gpioNum, GPIO.OUT)
+GPIO.setup(led_r_relay_gpioNum, GPIO.OUT)
+
+connectRelay(airPump_relay_gpioNum)
+connectRelay(led_relay_gpioNum)
+connectRelay(cooler_relay_gpioNum)
+connectRelay(led_r_relay_gpioNum)
+
+
+
+def checkTime(startTime, endTime):
+    '''
+    지금이 작동해야 하는 시간인지 판별하는 함수
+    stratTime, endTime은 hour와 minute으로 이루어진 정수형
+    ex) 15시 30분의 경우 1530
+    '''
+    currentTime = time.localtime(time.time())
+    currentTime = int(currentTime.tm_hour)*100 + int(currentTime.tm_min)
+    if startTime > endTime:
+        endTime += 2400
+    if startTime <= currentTime and currentTime < endTime:
+        return True
+    else:
+        return False
+
+
+def readGivenInfo():
+    '''
+    info.txt 파일에 저장된 값들 불러오기
+    '''
+    f=open("info.txt", "r")
+    lines=f.readlines() 
+    wanted_hum = int(lines[0])
+    wanted_temp = int(lines[1])
+    wanted_led_start_time = int(lines[2]) #8:30
+    wanted_led_end_time = int(lines[3]) #18:40
+    wanted_pump_start_time = int(lines[4]) #8:30
+    wanted_pump_end_time = int(lines[5]) #18:40
+    wanted_led_on_off = int(lines[6]) # 0은 False  1 True
+    wanted_led2_on_off = int(lines[7])
+    f.close()
+    return wanted_hum, wanted_temp, wanted_led_start_time, wanted_led_end_time, \
+           wanted_pump_start_time,  wanted_pump_end_time, wanted_led_on_off, wanted_led2_on_off
+
+def sendTempHum(c_temp, c_hum):
+    f=open("temphum.txt",'w')
+    f.write(str(c_temp)+"\n")
+    f.write(str(c_hum))
+    f.close()
+
+while True:
+    # get info
+    #print("working")
+    c_hum, c_temp = checkCurrentHumAndTemp()
+    #print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(c_temp, c_hum))
+    
+    sendTempHum(c_temp, c_hum)
+    wanted_hum, wanted_temp, wanted_led_start_time, wanted_led_end_time, wanted_pump_start_time, \
+    wanted_pump_end_time, wanted_led_on_off, wanted_led2_on_off = readGivenInfo()
+
+    # chcek temp and run cooler
+    if c_temp > wanted_temp:
+        connectRelay(cooler_relay_gpioNum)
+    else:
+        connectRelay(cooler_relay_gpioNum, False)
+
+    # check time and run LED
+    if checkTime(wanted_led_start_time, wanted_led_end_time):
+        if wanted_led_on_off == 1:
+            connectRelay(led_relay_gpioNum)
+        else:
+            connectRelay(led_relay_gpioNum, False)
+
+        if wanted_led2_on_off == 1:
+            connectRelay(led_r_relay_gpioNum)
+        else:
+            connectRelay(led_r_relay_gpioNum, False)
+    else:
+        connectRelay(led_relay_gpioNum, False)
+        connectRelay(led_r_relay_gpioNum, False)
+
+    # check time and run Pump
+    if checkTime(wanted_pump_start_time, wanted_pump_end_time):
+        connectRelay(airPump_relay_gpioNum)
+    else:
+        connectRelay(airPump_relay_gpioNum, False)
+
+
---
+++ workingfile_without_freezing/mark2.py
@@ -0,0 +1,245 @@
+# -*- coding: utf-8 -*-
+# Searching a key on a B-tree in Python
+# https://kookyungmin.github.io/study/2018/07/29/data_structure_02/
+import tkinter
+from tkinter import ttk
+from tkinter import *
+import tkinter.font
+
+#set GPIO
+#set gpioNum with BCM -1 = non set
+airPump_relay_gpioNum = 27
+led_relay_gpioNum = 23
+cooler_relay_gpioNum = 17
+led_r_relay_gpioNum = 24
+
+
+#start tk
+window = tkinter.Tk()
+
+minuteList = ['00', '01', '02', '03', '04', '05',
+              '06', '07', '08', '09', '10', '11',
+              '12', '13', '14', '15', '16', '17',
+              '18', '19', '20', '21', '22', '23',
+              '24', '25', '26', '27', '28', '29',
+              '30', '31', '32', '33', '34', '35',
+              '36', '37', '38', '39', '40', '41',
+              '42', '43', '44', '45', '46', '47',
+              '48', '49', '50', '51', '52', '53',
+              '54', '55', '56', '57', '58', '59']
+
+hourList = ['00', '01', '02', '03', '04', '05',
+            '06', '07', '08', '09', '10', '11',
+            '12', '13', '14', '15', '16', '17',
+            '18', '19', '20', '21', '22', '23']
+
+# title
+window.title("온습도 조회")
+window.geometry('800x480')
+window.resizable(True, True)
+# 폰트 설정
+fontStyle = tkinter.font.Font(size=24)
+buttonStyle = ttk.Style()
+buttonStyle.configure('my.TButton', font=(24))
+
+# %% 현 상황 조회창
+showTemp = ttk.Label(window, text="현재 온도 : ", font=fontStyle)
+showTemp.grid(column=0, row=0)
+
+showHum = ttk.Label(window, text="현재 습도 : ", font=fontStyle)
+showHum.grid(column=0, row=1)
+
+
+def settingActive():
+    '''설정 누르면 콤보박스 활성화 필요'''
+    # normal disable
+    setHumComboBox.configure(state='readonly')
+    setTempComboBox.configure(state='readonly')
+    setPumpStartTimeHourCombobox.configure(state='readonly')
+    setPumpStartTimeMinCombobox.configure(state='readonly')
+    setPumpEndTimeHourCombobox.configure(state='readonly')
+    setPumpEndTimeMinCombobox.configure(state='readonly')
+    setLEDStartTimeHourCombobox.configure(state='readonly')
+    setLEDStartTimeMinCombobox.configure(state='readonly')
+    setLEDEndTimeHourCombobox.configure(state='readonly')
+    setLEDEndTimeMinCombobox.configure(state='readonly')
+    setRedLED.configure(state='normal')
+    setWhiteLED.configure(state='normal')
+
+
+settingButton = ttk.Button(window, text="설정", style='my.TButton', command=settingActive)
+settingButton.grid(column=5, row=2)
+
+# %%온도 입력
+setTempLabel = ttk.Label(window, text="설정 온도 ( *C)", font=fontStyle)
+setTempLabel.grid(column=0, row=2)
+
+setTempComboBox = ttk.Combobox(window, width=5)
+setTempComboBox['values'] = [x for x in range(10, 31)]
+setTempComboBox.grid(column=1, row=2)
+setTempComboBox.current(0)
+
+# %%습도 입력
+setHumLabel = ttk.Label(window, text="설정 습도 ( % )",font=fontStyle)
+setHumLabel.grid(column=0, row=3)
+
+setHumComboBox = ttk.Combobox(window, width=5)
+setHumComboBox['values'] = [x for x in range(0, 101)]
+setHumComboBox.grid(column=1, row=3)
+setHumComboBox.current(50)
+
+# %% 펌프 시간 입력
+setPumpTimeLabel = ttk.Label(window, text="펌프 작동시간",font=fontStyle)
+setPumpTimeLabel.grid(column=0, row=4)
+
+setPumpStartTimeHourCombobox = ttk.Combobox(window, width=5)
+setPumpStartTimeHourCombobox['values'] = hourList
+setPumpStartTimeHourCombobox.grid(column=1, row=4)
+setPumpStartTimeHourCombobox.current(5)
+
+setPumpStartTimeMinCombobox = ttk.Combobox(window, width=5)
+setPumpStartTimeMinCombobox['values'] = minuteList
+setPumpStartTimeMinCombobox.grid(column=2, row=4)
+setPumpStartTimeMinCombobox.current(29)
+
+setPumpSplitLabel = ttk.Label(window, text="~",font=fontStyle)
+setPumpSplitLabel.grid(column=3, row=4)
+
+setPumpEndTimeHourCombobox = ttk.Combobox(window, width=5)
+setPumpEndTimeHourCombobox['values'] = hourList
+setPumpEndTimeHourCombobox.grid(column=4, row=4)
+setPumpEndTimeHourCombobox.current(18)
+
+setPumpEndTimeMinCombobox = ttk.Combobox(window, width=5)
+setPumpEndTimeMinCombobox['values'] = minuteList
+setPumpEndTimeMinCombobox.grid(column=5, row=4)
+setPumpEndTimeMinCombobox.current(29)
+
+# %% 조명 시간 입력
+
+setLEDTimeLabel = ttk.Label(window, text="조명 작동시간",font=fontStyle)
+setLEDTimeLabel.grid(column=0, row=5)
+
+setLEDStartTimeHourCombobox = ttk.Combobox(window, width=5)
+setLEDStartTimeHourCombobox['values'] = hourList
+setLEDStartTimeHourCombobox.grid(column=1, row=5)
+setLEDStartTimeHourCombobox.current(5)
+
+setLEDStartTimeMinCombobox = ttk.Combobox(window, width=5)
+setLEDStartTimeMinCombobox['values'] = minuteList
+setLEDStartTimeMinCombobox.grid(column=2, row=5)
+setLEDStartTimeMinCombobox.current(29)
+
+setLEDSplitLabel = ttk.Label(window, text="~",font=fontStyle)
+setLEDSplitLabel.grid(column=3, row=5)
+
+setLEDEndTimeHourCombobox = ttk.Combobox(window, width=5)
+setLEDEndTimeHourCombobox['values'] = hourList
+setLEDEndTimeHourCombobox.grid(column=4, row=5)
+setLEDEndTimeHourCombobox.current(18)
+
+setLEDEndTimeMinCombobox = ttk.Combobox(window, width=5)
+setLEDEndTimeMinCombobox['values'] = minuteList
+setLEDEndTimeMinCombobox.grid(column=5, row=5)
+setLEDEndTimeMinCombobox.current(30)
+
+# Define empty variables
+var1 = IntVar()
+var2 = IntVar()
+
+# %% 조명 선택      ////
+setWhiteLED = Checkbutton(window, text='1층', variable=var1, onvalue=1, offvalue=0, font=fontStyle)
+setWhiteLED.grid(column=1 , row=6)
+
+setRedLED = Checkbutton(window, text='2층', variable=var2, onvalue=1, offvalue=0, font=fontStyle)
+setRedLED.grid(column=2, row=6)
+
+# 초기 상태: normal disable
+setHumComboBox.configure(state='disable',font=fontStyle)
+setTempComboBox.configure(state='disable',font=fontStyle)
+setPumpStartTimeHourCombobox.configure(state='disable',font=fontStyle)
+setPumpStartTimeMinCombobox.configure(state='disable',font=fontStyle)
+setPumpEndTimeHourCombobox.configure(state='disable',font=fontStyle)
+setPumpEndTimeMinCombobox.configure(state='disable',font=fontStyle)
+setLEDStartTimeHourCombobox.configure(state='disable',font=fontStyle)
+setLEDStartTimeMinCombobox.configure(state='disable',font=fontStyle)
+setLEDEndTimeHourCombobox.configure(state='disable',font=fontStyle)
+setLEDEndTimeMinCombobox.configure(state='disable',font=fontStyle)
+setRedLED.configure(state='disable',font=fontStyle)
+setWhiteLED.configure(state='disable',font=fontStyle)
+
+
+# %% 사용자 설정 반영 버튼
+def apply():
+    setHumComboBox.configure(state='disable')
+    setTempComboBox.configure(state='disable')
+    setPumpStartTimeHourCombobox.configure(state='disable')
+    setPumpStartTimeMinCombobox.configure(state='disable')
+    setPumpEndTimeHourCombobox.configure(state='disable')
+    setPumpEndTimeMinCombobox.configure(state='disable')
+    setLEDStartTimeHourCombobox.configure(state='disable')
+    setLEDStartTimeMinCombobox.configure(state='disable')
+    setLEDEndTimeHourCombobox.configure(state='disable')
+    setLEDEndTimeMinCombobox.configure(state='disable')
+    setRedLED.configure(state='disable')
+    setWhiteLED.configure(state='disable')
+
+    # 여기서부터 다른 파일로 전달 혹은 서로 전송 필요
+    f = open("info.txt", "w")
+    f.write(setHumComboBox.get() + "\n")
+    f.write(setTempComboBox.get() + "\n")
+    f.write(setLEDStartTimeHourCombobox.get() + setLEDStartTimeMinCombobox.get() + "\n")
+    f.write(setLEDEndTimeHourCombobox.get() + setLEDEndTimeMinCombobox.get() + "\n")
+    f.write(setPumpStartTimeHourCombobox.get() + setPumpStartTimeMinCombobox.get() + "\n")
+    f.write(setPumpEndTimeHourCombobox.get() + setPumpEndTimeMinCombobox.get() + "\n")
+    f.write('%s\n' % var1.get())
+    f.write('%s\n' % var2.get())
+
+    f.close()
+
+
+applyButton = ttk.Button(window, text='적용', style='my.TButton', command=apply)
+applyButton.grid(column=5, row=6)
+
+def end():
+    exit(0)
+
+endButton = ttk.Button(window, text='전체 종료', style='my.TButton', command=end)
+endButton.grid(column=5, row=0)
+
+def RPcontrol():
+  
+    # %% 현재 온습도 조회 , strip사용
+
+    f = open("temphum.txt", 'r')
+    lines = f.readlines()
+    showTemp.config(text="현재 온도: " + lines[0].strip() +"*C")
+    showHum.config(text="현재 습도: " + lines[1] +'%')
+    
+    f.close()
+    window.after(100, RPcontrol)
+
+# %% 최종
+RPcontrol()
+
+window.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
